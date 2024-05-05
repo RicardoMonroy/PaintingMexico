7709278751d1766f6b0a4ae250dc96e7741e196b
Ricardo Monroy <rmonroy.rodriguez@gmail.com>
2024-05-04 18:44:00 -0600

    Importación repetida

--- resources/js/Pages/ViewSale.jsx
+++ resources/js/Pages/ViewSale.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import config from '@/config';
 import axios from 'axios';
-import config from '@/config';
 
 function ViewSale({ viewId }) {
     const [saleData, setSaleData] = useState(null);
