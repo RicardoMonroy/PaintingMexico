1e3d8fecc83c7dc3d904b7809dd3589583366dc7
Ricardo Monroy <rmonroy.rodriguez@gmail.com>
2024-09-28 13:01:05 -0600

    config

--- resources/js/config.jsx
+++ resources/js/config.jsx
@@ -1,7 +1,7 @@
 // resources/js/config.js
 const config = {
-    // API_URL: 'https://paintingmexico.com/api'
-    API_URL: 'http://127.0.0.1:8000/api'
+    API_URL: 'https://paintingmexico.com/api'
+    // API_URL: 'http://127.0.0.1:8000/api'
   };
   
   export default config;
